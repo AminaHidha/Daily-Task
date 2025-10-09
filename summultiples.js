6d8e36e8f2cbaf04bfe7158a3f075b4e32a21611
Hidha2004 <hidhahidha03@gmail.com>
2025-10-09 19:22:49 +0530

    summultiples

---
+++ summultiples.js
@@ -0,0 +1,12 @@
+var sumOfMultiples = function(n) {
+  let total = 0;
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 || i % 5 === 0 || i % 7 === 0) {
+      total += i;
+    }
+  }
+  return total;
+};
+console.log(sumOfMultiples(7));
+console.log(sumOfMultiples(10));
+console.log(sumOfMultiples(9));

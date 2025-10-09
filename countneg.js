2dcb81d8aee2bf98334328876deaa03cde70ae51
Hidha2004 <hidhahidha03@gmail.com>
2025-10-09 15:13:46 +0530

    smallthancurrent

---
+++ countneg.js
@@ -0,0 +1,7 @@
+var countNegatives = function(grid) {
+ let count=0
+ for(let )
+    
+};
+console.log(countNegatives( [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]));
+console.log(countNegatives( [[3,2],[1,0]]));

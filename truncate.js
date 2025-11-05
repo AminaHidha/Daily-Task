0a2bc0f892d6bb4abb54a1b898278d67a15acbfd
Hidha2004 <hidhahidha03@gmail.com>
2025-11-05 10:41:05 +0530

    truncate

--- truncate.js
+++ truncate.js
@@ -1,4 +1,11 @@
 var truncateSentence = function(s, k) {
+     val1=s.split(' ')
+     console.log(val1);  
+    val=val1.splice(k,)
+    console.log(val);
     
+    return val1.join(" ")
+  
+
 };
-console.log(truncateSentence("Hello how are you Contestant", k = 4));
+console.log(truncateSentence("Hello how are you Contestant",4));

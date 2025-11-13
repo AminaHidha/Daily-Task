8fbb10a366f96e4ca2c92ced4598ac59befa5105
Hidha2004 <hidhahidha03@gmail.com>
2025-11-13 10:13:27 +0530

    emprature

---
+++ temprature.js
@@ -0,0 +1,6 @@
+var convertTemperature = function(celsius) {
+    let res=[Kelvin = celsius + 273.15,
+    Fahrenheit = celsius * 1.80 + 32.00]
+    return res
+};
+console.log(convertTemperature(36.50));
